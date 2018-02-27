28fffdcd9d298998160a709bb95c3cc879c02053
tyler <hisday@sk.com>
2018-02-27 19:33:28 +0900

    implemented search function

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#veffjq#^xuna=q&o6z9-va!#4ih4_0i2*%unr)x3p$jpy0x81'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = '*'
 
